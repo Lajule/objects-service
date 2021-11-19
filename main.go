4398ea17d7977d9bc3b67f558c37dee562c59b72
Lajule <julien.rouzieres@mac.com>
2021-11-19 01:06:24 +0100

    Better logs

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func main() {
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Fatal("listening", zap.Error(err))
+			logger.Fatal("listening...", zap.Error(err))
 		}
 	}()
 
@@ -71,7 +71,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		logger.Fatal("Server forced to shutdown: ", zap.Error(err))
+		logger.Fatal("Server forced to shutdown", zap.Error(err))
 	}
 
 	logger.Info("Server exiting")
