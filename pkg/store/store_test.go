259445fcae65a1829437aca8301b1f42e3009dd9
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2021-12-30 10:56:56 +0100

    Review test

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -9,57 +9,45 @@ import (
 	"github.com/Lajule/objects-service/pkg/store"
 )
 
-var (
-	logger *zap.Logger
-
-	st *store.Store
-)
-
-func init() {
-	logger, _ = zap.NewProduction()
-	st = store.NewStore("test", true, logger)
-}
-
-func TestCreateBucketIfNotExists(t *testing.T) {
+func TestStore(t *testing.T) {
 	is := is.New(t)
 
-	err := st.CreateBucketIfNotExists("bucket")
-	is.NoErr(err)
-}
+	logger, _ := zap.NewProduction()
+	st := store.NewStore("test", true, logger)
 
-func TestCreateOrOpenObject(t *testing.T) {
-	is := is.New(t)
+	t.Run("CreateBucketIfNotExists", func(t *testing.T) {
+		err := st.CreateBucketIfNotExists("bucket")
+		is.NoErr(err)
+	})
 
-	_, err := st.CreateOrOpenObject("bucket", "object")
-	is.NoErr(err)
-}
+	t.Run("CreateOrOpenObject", func(t *testing.T) {
+		_, err := st.CreateOrOpenObject("bucket", "object")
+		is.NoErr(err)
+	})
 
-func TestGetObjectIfExists(t *testing.T) {
-	is := is.New(t)
+	t.Run("GetObjectIfExists", func(t *testing.T) {
+		_, err := st.CreateOrOpenObject("bucket", "object")
+		is.NoErr(err)
 
-	_, err := st.CreateOrOpenObject("bucket", "object")
-	is.NoErr(err)
+		f, err := st.GetObjectIfExists("bucket", "object")
+		is.NoErr(err)
+		is.True(f != nil)
 
-	f, err := st.GetObjectIfExists("bucket", "object")
-	is.NoErr(err)
-	is.True(f != nil)
+		f2, err := st.GetObjectIfExists("bucket", "object2")
+		is.NoErr(err)
+		is.True(f2 == nil)
+	})
 
-	f2, err := st.GetObjectIfExists("bucket", "object2")
-	is.NoErr(err)
-	is.True(f2 == nil)
-}
+	t.Run("RemoveObjectIfExists", func(t *testing.T) {
+		_, err := st.CreateOrOpenObject("bucket", "object")
+		is.NoErr(err)
 
-func TestRemoveObjectIfExists(t *testing.T) {
-	is := is.New(t)
+		removed, err := st.RemoveObjectIfExists("bucket", "object")
+		is.NoErr(err)
+		is.True(removed)
 
-	_, err := st.CreateOrOpenObject("bucket", "object")
-	is.NoErr(err)
-
-	removed, err := st.RemoveObjectIfExists("bucket", "object")
-	is.NoErr(err)
-	is.True(removed)
-
-	removed, err = st.RemoveObjectIfExists("bucket", "object")
-	is.NoErr(err)
-	is.True(!removed)
+		removed, err = st.RemoveObjectIfExists("bucket", "object")
+		is.NoErr(err)
+		is.True(!removed)
+	})
 }
